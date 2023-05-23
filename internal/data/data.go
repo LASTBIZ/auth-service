970fb229e1183228336905daa46d8b525f65b8d3
1Suro1 <timu.zaxarov@gmail.com>
2023-05-23 17:06:11 +0900

    init

---
+++ internal/data/data.go
@@ -0,0 +1,46 @@
+package data
+
+import (
+	"auth-service/api/user"
+	"auth-service/internal/conf"
+	"context"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/tracing"
+	"github.com/go-kratos/kratos/v2/transport/grpc"
+	"time"
+
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+)
+
+// ProviderSet is data providers.
+var ProviderSet = wire.NewSet(NewData, NewGreeterRepo)
+
+// Data .
+type Data struct {
+	// TODO wrapped database client
+}
+
+// NewData .
+func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
+	cleanup := func() {
+		log.NewHelper(logger).Info("closing the data resources")
+	}
+	return &Data{}, cleanup, nil
+}
+
+func NewUserServiceClient(ac *conf.Auth, sr *conf.Service) user.UserClient {
+	conn, err := grpc.DialInsecure(
+		context.Background(),
+		grpc.WithEndpoint(sr.User.Endpoint),
+		grpc.WithMiddleware(
+			tracing.Client(),
+			recovery.Recovery()),
+		grpc.WithTimeout(2*time.Second),
+	)
+	if err != nil {
+		panic(err)
+	}
+	c := user.NewUserClient(conn)
+	return c
+}
